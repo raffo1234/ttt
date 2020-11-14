f8c5a516aa500324572bc03cde2cb683f01a968e
Rafael Meza <rhmcord@gmail.com>
2020-11-13 23:11:24 -0500

    Set players

--- ttt.rb
+++ ttt.rb
@@ -7,12 +7,35 @@ class Board
   end
 end
 
+class Coordinate
+  attr_reader :xy
+  def initialize(xy)
+    @xy = xy
+  end
+end
+
+class Player
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+end
+
 class Game
+  def set_players
+    print "Insert player 1: "
+    name_1 = gets.chomp
+    player_1 = Player.new(name_1)
+    print "Insert player 2: "
+    name_2 = gets.chomp
+    player_2 = Player.new(name_2)
+  end
+  
   def start
     Board.new.draw
   end
 end
 
 
-
-game1 = Game.new.start
+game1 = Game.new
+game1.set_players
