fde1a4ae4a25e1b27247370b66768991b4a111b5
Rafael Meza <rhmcord@gmail.com>
2020-11-14 11:53:43 -0500

    Get mark and create play_next

--- ttt.rb
+++ ttt.rb
@@ -1,9 +1,28 @@
 class Board
+  attr_accessor :coordinates
+
+  def initialize(coordinates = {})
+    coordinates["a1"] = " "
+    coordinates["a2"] = " "
+    coordinates["a3"] = " "
+    coordinates["b1"] = " "
+    coordinates["b2"] = " "
+    coordinates["b3"] = " "
+    coordinates["c1"] = " "
+    coordinates["c2"] = " "
+    coordinates["c3"] = " "
+    @coordinates = coordinates
+  end
+
   def draw
-    print "    1  |  2 |  3 \n"
-    print "a  ____|____|____\n"
-    print "b  ____|____|____\n"
-    print "c      |    |    \n"
+    print "    1     2    3 \n"
+    print " \n"
+    print "a    #{coordinates["a1"]} |  #{coordinates["a2"]} |  #{coordinates["a3"]}  \n"
+    print "   ____|____|____\n"
+    print "b    #{coordinates["b1"]} |  #{coordinates["b2"]} |  #{coordinates["b3"]}  \n"
+    print "   ____|____|____\n"
+    print "c    #{coordinates["c1"]} |  #{coordinates["c2"]} |  #{coordinates["c3"]}  \n"
+    print "       |    |    \n"
   end
 end
 
@@ -15,27 +34,48 @@ class Coordinate
 end
 
 class Player
-  attr_accessor :name
+  attr_accessor :name, :coordinate
   def initialize(name)
     @name = name
+  end
+
+  def get_mark
+    print "Type a coordinate: "
+    @coordinate = gets.chomp
   end
 end
 
 class Game
-  def set_players
-    print "Insert player 1: "
-    name_1 = gets.chomp
-    player_1 = Player.new(name_1)
-    print "Insert player 2: "
-    name_2 = gets.chomp
-    player_2 = Player.new(name_2)
+  def initialize(game_coordinates = [])
+    @game_coordinates = game_coordinates
   end
-  
+
+  def set_player
+    print "Insert player: "
+    @name = gets.chomp
+    @player = Player.new(@name)
+  end
+
   def start
-    Board.new.draw
+    @board = Board.new
+    @board.draw
+  end
+
+  def play_next(mark)
+    @player.get_mark
+    @game_coordinates << @player.coordinate
+    @board.coordinates[@player.coordinate] = mark
+    puts `clear`
+    @board.draw
   end
 end
 
-
 game1 = Game.new
-game1.set_players
+
+# Set name of players
+game1.set_player 
+
+game1.start
+
+# Loop until finding a winner
+game1.play_next("X")
